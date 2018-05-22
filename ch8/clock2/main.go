fb58ce4346e1022a1697479925689826a177df36
José Guilherme Vanz <guilherme.sft@gmail.com>
2018-05-21 23:34:00 -0300

    clock2: -port -tz command line flags
    Updates the clock2 program to allow the user set the listen port and the
    timezone which the time server will return the time
    
    Signed-off-by: José Guilherme Vanz <guilherme.sft@gmail.com>

--- ch8/clock2/main.go
+++ ch8/clock2/main.go
@@ -1,17 +1,25 @@
 package main
 
 import (
+	"flag"
 	"io"
 	"log"
 	"net"
+	"strconv"
 	"time"
 )
 
+var port = flag.Uint64("port", 8000, "Listen port")
+var timezone = flag.String("tz", "Local", "Time zone")
+
 func main() {
-	listener, err := net.Listen("tcp", "localhost:8000")
+	// get command lines flags
+	flag.Parse()
+	listener, err := net.Listen("tcp", "127.0.0.1:"+strconv.FormatUint(*port, 10))
 	if err != nil {
 		log.Fatal(err)
 	}
+	log.Print("Listening ", listener.Addr())
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
@@ -24,8 +32,12 @@ func main() {
 
 func handleConn(c net.Conn) {
 	defer c.Close()
+	loc, err := time.LoadLocation(*timezone)
+	if err != nil {
+		log.Fatal(err)
+	}
 	for {
-		_, err := io.WriteString(c, time.Now().Format("15:04:05\n"))
+		_, err := io.WriteString(c, time.Now().In(loc).Format("15:04:05\n"))
 		if err != nil {
 			return
 		}
